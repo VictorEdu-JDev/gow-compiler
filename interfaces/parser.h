540e2e30ce4c789a0a50bd88a668d14986c6ad1f
Victor Eduardo <victorepc.dev@gmail.com>
2025-02-23 15:00:03 -0300

    Visibiliza constantes de nós de árvore AST; Define estrutura de nó reutilizável.

--- interfaces/parser.h
+++ interfaces/parser.h
@@ -2,8 +2,34 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-typedef struct ast_node ASTNode;
+enum node_type {
+    NODE_FUNCTION,
+    NODE_RETURN,
+    NODE_CALL,
+    NODE_ASSIGN,
+    NODE_IF,
+    NODE_ELSE,
+    NODE_ELSEIF,
+    NODE_FOR,
+    NODE_BREAK,
+    NODE_OPERATOR,
+    NODE_IDENTIFIER,
+    NODE_NUMBER,
+    NODE_FLOAT,
+    NODE_DOUBLE,
+    NODE_BOOLEAN,
+    NODE_STRING
+};
+
 typedef enum node_type NodeType;
+typedef struct ast_node ASTNode;
+
+struct ast_node {
+    NodeType type;
+    ASTNode *left;
+    ASTNode *right;
+    char value[255];
+};
 
 ASTNode *parse();
 
