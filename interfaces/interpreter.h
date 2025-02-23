f88c01d6cacbd6aafa2fef60d6089865d074c24a
Victor Eduardo <victorepc.dev@gmail.com>
2025-02-23 14:59:07 -0300

    Define estruturas e macros comuns.

--- interfaces/interpreter.h
+++ interfaces/interpreter.h
@@ -1,8 +1,21 @@
 
 #ifndef INTERPRETER_H
 #define INTERPRETER_H
+#include "lexer.h"
 
-void addVariable(const char *name, int value);
-void printVariable(const char *name);
+#define MAX_VARIABLES 1000
+#define MAX_FUNCTIONS 1000
+
+#define MAX_IDENTIFIER_LENGTH 100
+#define MAX_TOKENS 100
+
+
+typedef struct variable Variable;
+typedef struct function Function;
+typedef struct environment Environment;
+
+void callFunction(const char *name);
+void executeFunction(Function *function);
+void addVariable(const char *name, void *value, TokenType type);
 
 #endif //INTERPRETER_H
